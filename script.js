6a00363bc358a0c1f0c045d6334cd091163e0790
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2024-11-29 19:35:15 +0530

    deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector("#menu-icon");
-const navbar = document.querySelector(".navbar");
-const navbg = document.querySelector(".navbg");
-
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-  navbg.classList.toggle("active");
-});
